061d6624f27450ce440fc9d1a7b5ffd0238b059d
loganlebanoff <loganlebanoff@yahoo.com>
2017-06-04 01:18:44 -0400

    Working seq2seq example for simply reversing the inputs. Does well qualitatively on test set.

--- code/test.py
+++ code/test.py
@@ -1,40 +1,65 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-
-from __future__ import print_function
 import numpy as np
 import tensorflow as tf
-from tensorflow.contrib.rnn.python.ops import core_rnn_cell
 from seq2seq import basic_rnn_seq2seq
-# Declare list of features, we only have one real-valued feature
-def model(features, labels, mode):
-  cell = tf.contrib.rnn.BasicRNNCell(2)
-#   y, _ = basic_rnn_seq2seq(features['x'], labels, cell)
-  y, states = tf.contrib.rnn.static_rnn(cell, [features['x']], dtype=tf.float32)
-  y = y[-1]
-  
-  # Loss sub-graph
-  loss = tf.reduce_sum(tf.square(y - labels))
-  # Training sub-graph
-  global_step = tf.train.get_global_step()
-  optimizer = tf.train.GradientDescentOptimizer(0.01)
-  train = tf.group(optimizer.minimize(loss),
-                   tf.assign_add(global_step, 1))
-  # ModelFnOps connects subgraphs we built to the
-  # appropriate functionality.
-  return tf.contrib.learn.ModelFnOps(
-      mode=mode, predictions=y,
-      loss=loss,
-      train_op=train)
+from tensorflow.contrib.rnn import BasicRNNCell
 
-estimator = tf.contrib.learn.Estimator(model_fn=model)
-# define our data set
-x = np.array([[1.,1.], [2.,2.], [3.,3.], [4.,4.]])
-y = np.array([[0.,0.], [-1.,-1.], [-2.,-2.], [-3.,-3.]])
-input_fn = tf.contrib.learn.io.numpy_input_fn({"x": x}, y, 4, num_epochs=1000)
+prediction_file = 'predictions.txt'
+y_file = 'y.txt'
 
-# train
-estimator.fit(input_fn=input_fn, steps=1000)
-# evaluate our model
-print(estimator.evaluate(input_fn=input_fn, steps=10))
+EPOCHS = 10000
+PRINT_STEP = 1000
+NUM_TRAIN = 1000
+NUM_TEST = 1000
+SEQUENCE_LEN = 20
+VOCAB_SIZE = 1000
+np.random.seed(123)
+
+# Each input instance is a sequence of ints. 
+train_x = np.random.randint(VOCAB_SIZE, size=(NUM_TRAIN, SEQUENCE_LEN))
+# Each target is the sequence in reverse.
+train_y = np.fliplr(train_x)
+
+test_x = np.random.randint(VOCAB_SIZE, size=(NUM_TEST, SEQUENCE_LEN))
+test_y = np.fliplr(test_x)
+
+data = train_x
+target = train_y
+
+x_ = tf.placeholder(tf.float32, [None, data.shape[1]])
+y_ = tf.placeholder(tf.float32, [None, target.shape[1]])
+
+cell = BasicRNNCell(num_units=data.shape[1], activation=tf.nn.relu)
+
+# outputs, states = tf.contrib.rnn.static_rnn(cell, [x_], dtype=tf.float32)
+y, states = basic_rnn_seq2seq([x_], [y_], cell)
+y = y[-1]
+
+cost = tf.reduce_mean(tf.square(y - y_))
+train_op = tf.train.RMSPropOptimizer(0.005, 0.2).minimize(cost)
+
+with tf.Session() as sess:
+    tf.initialize_all_variables().run()
+    for i in range(EPOCHS):
+        # train for 1 epoch
+        sess.run(train_op, feed_dict={x_:data, y_:target})
+        
+        # print loss
+        if i % PRINT_STEP == 0:
+            c = sess.run(cost, feed_dict={x_:data, y_:target})
+            print('training cost:', c)
+
+    # test on test set
+    response = sess.run(y, feed_dict={x_:test_x, y_:test_y})
+    response = np.round(response)
+    
+    #print to files
+    with open(prediction_file, 'w') as f:
+        np.savetxt(f, response, fmt='%d\t')
+    with open(y_file, 'w') as f:
+        np.savetxt(f, test_y, fmt='%d\t')
+    
+    
+    
+    
+    
+    
