d733e6df6c9e3171832204773a8b33e5bd06e155
loganlebanoff <loganlebanoff@yahoo.com>
2017-06-20 12:02:40 -0400

    Fixed the decoder inputs. It now feeds in the PREVIOUS groundtruth, not the current one. Also added tensorboard code, and GRU support

--- code/test_decoder.py
+++ code/test_decoder.py
@@ -1,25 +1,31 @@
 import numpy as np
 import tensorflow as tf
 from seq2seq import basic_rnn_seq2seq, rnn_decoder, embedding_rnn_decoder, sequence_loss
-from tensorflow.contrib.rnn import BasicRNNCell
+from tensorflow.contrib.rnn import BasicRNNCell, BasicLSTMCell, GRUCell
+from metrics import performance
 
 prediction_file = 'predictions.txt'
 y_file = 'y.txt'
 summary_file = '/home/logan/tmp'
 
-EPOCHS = 500
+EPOCHS = 5000
 PRINT_STEP = 1
-NUM_TRAIN = 1500
-NUM_TEST = 1500
-LATENT_SIZE = 10
+NUM_TRAIN = 10000
+NUM_TEST = 1000
+LATENT_SIZE = 5
 SEQUENCE_LEN = 5
-VOCAB_SIZE = 1000
-EMBEDDING_SIZE = 100
-PATIENCE = 10
+VOCAB_SIZE = 10
+EMBEDDING_SIZE = 50
+PATIENCE = 20
+cell_type = 1
 np.random.seed(123)
 
+if cell_type == 2:
+    len_x = LATENT_SIZE/2
+else:
+    len_x = LATENT_SIZE
 # Each input is a latent vector of size LATENT_SIZE
-x = np.random.randint(VOCAB_SIZE, size=(NUM_TRAIN+NUM_TEST, LATENT_SIZE))
+x = np.random.randint(1, VOCAB_SIZE, size=(NUM_TRAIN+NUM_TEST, len_x))
 # Each target is a sequence based on the latent vector
 y = np.zeros((NUM_TRAIN+NUM_TEST, SEQUENCE_LEN))
 # sum of each row
@@ -37,29 +43,53 @@ for r in range(len(y)):
 #         else:
 #             previous = y[r,c-1]
 #         y[r,c] = temp + previous
+
+y_shifted = np.roll(y, 1)
+y_shifted[:,0] = 0
     
 train_x = x[:NUM_TRAIN]
 train_y = y[:NUM_TRAIN]
+train_y_shifted = y_shifted[:NUM_TRAIN]
 
 test_x = x[NUM_TRAIN:]
 test_y = y[NUM_TRAIN:]
+test_y_shifted = y_shifted[NUM_TRAIN:]
 
 data = train_x
 target = train_y
 
+def variable_summaries(var):
+  """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
+  with tf.name_scope('summaries'):
+    mean = tf.reduce_mean(var)
+    tf.summary.scalar('mean', mean)
+    with tf.name_scope('stddev'):
+      stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+    tf.summary.scalar('stddev', stddev)
+    tf.summary.scalar('max', tf.reduce_max(var))
+    tf.summary.scalar('min', tf.reduce_min(var))
+    tf.summary.histogram('histogram', var)
+
 x_ = tf.placeholder(tf.float32, [None, data.shape[1]], name='x_')
+c_init = tf.fill(tf.shape(x_), 0.0)
 y_ = [tf.placeholder(tf.int64, [None,], name='y_' + str(i)) for i in xrange(target.shape[1])]
+inputs = [tf.placeholder(tf.int64, [None,], name='input_' + str(i)) for i in xrange(target.shape[1])]
 weights = [tf.fill(tf.shape(y_[0]), 1.0) for i in xrange(target.shape[1])]
-# feed_previous = tf.placeholder(tf.int64, name='feed_previous')
-# _feed_previous = (feed_previous == 1)
 
-cell = BasicRNNCell(num_units=data.shape[1], activation=tf.nn.sigmoid)
+if cell_type == 2:
+    cell = BasicLSTMCell(num_units=data.shape[1], activation=tf.nn.tanh, state_is_tuple=False)
+elif cell_type == 0:
+    cell = BasicRNNCell(num_units=data.shape[1], activation=tf.nn.tanh)
+elif cell_type == 1:
+    cell = GRUCell(num_units=data.shape[1], activation=tf.nn.tanh)
 
-W = tf.Variable(tf.random_normal([LATENT_SIZE, VOCAB_SIZE]), name='W')     
-b = tf.Variable(tf.random_normal([VOCAB_SIZE]), name='b')
+W = tf.Variable(tf.random_normal([LATENT_SIZE, VOCAB_SIZE]), name='W')    
+b = tf.Variable(tf.random_normal([VOCAB_SIZE]), name='b')    
+variable_summaries(W) 
+variable_summaries(b)
 
-with tf.variable_scope("foo"):
-    outputs, states = embedding_rnn_decoder(y_,
+with tf.variable_scope("decoder"):
+    outputs, states = embedding_rnn_decoder(inputs,
                               x_,
                               cell,
                               VOCAB_SIZE,
@@ -68,7 +98,7 @@ with tf.variable_scope("foo"):
                               feed_previous=False,
                               update_embedding_for_previous=True)
     tf.get_variable_scope().reuse_variables()
-    outputs_fed_previous, states_fed_previous = embedding_rnn_decoder(y_,
+    outputs_fed_previous, states_fed_previous = embedding_rnn_decoder(inputs,
                               x_,
                               cell,
                               VOCAB_SIZE,
@@ -95,13 +125,18 @@ cost, y = calcCost(y_, outputs)
 cost_fed_previous, y_fed_previous = calcCost(y_, outputs_fed_previous)
 # test_cost = calcCost(y, test_outputs)
 train_op = tf.train.RMSPropOptimizer(0.005, 0.2).minimize(cost)
+merged = tf.summary.merge_all()
 
 with tf.Session() as sess:
     train_writer = tf.summary.FileWriter(summary_file + '/train', sess.graph)
-    tf.initialize_all_variables().run()
+    tf.global_variables_initializer().run()
     train_dict = {i: d for i, d in zip(y_, np.transpose(target))}
+    temp = {i: d for i, d in zip(inputs, np.transpose(train_y_shifted))}
+    train_dict.update(temp)
     train_dict[x_] = data
     test_dict = {i: d for i, d in zip(y_, np.transpose(test_y))}
+    temp = {i: d for i, d in zip(inputs, np.transpose(test_y_shifted))}
+    test_dict.update(temp)
     test_dict[x_] = test_x
     min_test_cost = np.inf
     num_mistakes = 0
@@ -111,7 +146,8 @@ with tf.Session() as sess:
         
         # print loss
         if i % PRINT_STEP == 0:
-            train_cost = sess.run(cost, feed_dict=train_dict)
+            train_cost, summary = sess.run([cost, merged], feed_dict=train_dict)
+            train_writer.add_summary(summary, i)
             test_cost = sess.run(cost_fed_previous, feed_dict=test_dict)
             print('training cost:', train_cost, 'test cost:', test_cost)
         if test_cost < min_test_cost:
@@ -121,11 +157,22 @@ with tf.Session() as sess:
             num_mistakes += 1
         if num_mistakes >= PATIENCE:
             break
+        
+    train_writer.close()
+        
+    # test on train set
+    response = sess.run(y_fed_previous, feed_dict=train_dict)
+    response = np.array(response)
+    response = np.transpose(response)
+    acc,_,_,_ = performance(response, train_y)
+    print('Train accuracy: ', acc)
+    
     # test on test set
     response = sess.run(y_fed_previous, feed_dict=test_dict)
     response = np.array(response)
     response = np.transpose(response)
-    
+    acc,_,_,_ = performance(response, test_y)
+    print('Test accuracy: ', acc)
     #print to files
     with open(prediction_file, 'w') as f:
         np.savetxt(f, response, fmt='%d\t')
